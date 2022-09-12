89705409591260c654f3b7d3946c96b12b5fad69
carlos <carlos.av.amorim@gmail.com>
2022-09-12 20:50:03 -0300

    Feat: Create components and colors on Tailwind configs

---
+++ src/Pages/Event.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+const Event: React.FC = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Event;
---
+++ src/components/Lesson/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Lesson: React.FC = () => {
+  return <div />;
+};
+
+export default Lesson;

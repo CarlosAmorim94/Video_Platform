936426550d7d72bd71a65a71cc7db516a3f8b12c
carlos <carlos.av.amorim@gmail.com>
2022-09-13 13:11:31 -0300

    Style: Stye of Video components done

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,4 +1,9 @@
-import { DiscordLogo, Lightning } from "phosphor-react";
+import {
+  CaretRight,
+  DiscordLogo,
+  FileArrowDown,
+  Lightning,
+} from "phosphor-react";
 
 const Video = () => {
   return (
@@ -12,12 +17,27 @@ const Video = () => {
             <h1 className="text-2xl font-bold">
               Aula 01 - Abertura do ignite lab
             </h1>
-            <p className="mt-4 text-gray-200">
+            <p className="mt-4 text-gray-200 leading-relaxed">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia,
               rem. Alias impedit doloremque dolorum incidunt in obcaecati nam,
               magnam magni ipsa qui amet, cumque omnis repudiandae! Sint
               sapiente atque ut.
             </p>
+            <div className="flex items-center gap-4 mt-6">
+              <img
+                className="h-16 w-16 rounded-full border-2 border-blue-500"
+                src="https://github.com/carlosamorim94.png"
+                alt="imagem de perfil"
+              />
+              <div className="leading-relaxed">
+                <strong className="font-bold text-2xl block">
+                  Carlos Amorim
+                </strong>
+                <span className="text-gray-200 text-sm block">
+                  Front-End developer
+                </span>
+              </div>
+            </div>
           </div>
           <div className="flex flex-col gap-4">
             <a
@@ -36,8 +56,44 @@ const Video = () => {
             </a>
           </div>
         </div>
+        <div className="gap-8 mt-20 grid grid-cols-2">
+          <a
+            href="#"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Material complementar</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Acesse o material complementar para acelerar seu desenvolvimento
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+          <a
+            href="#"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+          >
+            <div className="bg-green-700 h-full p-6 flex items-center">
+              <FileArrowDown size={40} />
+            </div>
+            <div className="py-6 leading-relaxed">
+              <strong className="text-2xl">Wallpapers Exclusivos</strong>
+              <p className="text-sm text-gray-200 mt-2">
+                Baixe wallpapers exclusivos do Ignite Lab e personalize sua
+                máquina!
+              </p>
+            </div>
+            <div className="h-full p-6 flex items-center">
+              <CaretRight size={24} />
+            </div>
+          </a>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
